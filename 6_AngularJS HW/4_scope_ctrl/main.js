0a94ddbecfdef389fd178a46e8598a41eefae103
RAS <r.a.s@live.ru>
2018-02-26 11:30:59 +0200

    Some changes

--- 6_AngularJS HW/4_scope_ctrl/main.js
+++ 6_AngularJS HW/4_scope_ctrl/main.js
@@ -1,51 +1,71 @@
-$(function () {
+$(function() {
   $('#group1').buttonset();
-  $('#radio1, #radio2, #radio3').checkboxradio({
+  $('#radio1, #radio2, #radio3').button({
     icon: false
   });
 
-  $('#radio1').button().click(function (e) {
-    angular
-      .element(angularRegion)
-      .scope()
-      .$apply('addUser()');
+  $('#radio1').click(function() {
+    angular.element(angularRegion).scope().$apply('showNewUser()');
   });
-  $('#radio1').button().click(function (e) {
-    angular
-      .element(angularRegion)
-      .scope()
-      .$apply('delUser()');
+  $('#radio2').click(function() {
+    angular.element(angularRegion).scope().$apply('deleteUser()');
   });
-  $('#radio1').button().click(function (e) {
-    angular
-      .element(angularRegion)
-      .scope()
-      .$apply('editUser()');
+  $('#radio3').click(function() {
+    angular.element(angularRegion).scope().$apply('editSomeUser()');
   });
 });
-var model = [{
-  name: 'Петя',
-  surname: 'Пяточкин'
-},
-{
-  name: 'Жора',
-  surname: 'Петрович'
-},
-{
-  name: 'Василиса',
-  surname: 'Примудрая'
-}
+
+var model = [
+  {
+    name: 'Петя',
+    surname: 'Пяточкин'
+  },
+  {
+    name: 'Жора',
+    surname: 'Петрович'
+  },
+  {
+    name: 'Василиса',
+    surname: 'Примудрая'
+  }
 ];
-angular.module('formApp', []).controller('mainCtrl', function ($scope) {
+
+angular.module('formApp', []).controller('mainCtrl', function($scope) {
   $scope.users = model;
-  $scope.addUser = function () {
+  $scope.delUsers = false;
+  $scope.editUsers = false;
+  $scope.showNewUsers = false;
+  
+  $scope.showNewUser = function() {
+    $scope.showNewUsers = true;
+    $scope.delUsers = false;
+    $scope.editUsers = false;
+  };
+
+  $scope.deleteUser = function() {
+    $scope.delUsers = true;
+    $scope.showNewUsers = false;
+    $scope.editUsers = false;
+  };
+
+  $scope.editSomeUser = function() {
+    $scope.editUsers = true;
+    $scope.showNewUsers = false;
+    $scope.delUsers = false;
+  };
+
+  $scope.addUser = function() {
     $scope.users.push({
       name: $scope.newName,
       surname: $scope.newSurname
     });
   };
-  $scope.delUser = function () {
-    $scope.users;
+
+  $scope.delUser = function(i) {
+    $scope.users.splice([i], 1);
   };
-  $scope.editUser = function () {};
+
+  $scope.editUser = function() {
+    $scope.users.splice([i], 1);
+  };
 });
