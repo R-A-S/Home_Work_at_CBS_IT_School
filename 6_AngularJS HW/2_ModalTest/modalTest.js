f374d48550b96f6b0ffdeb32d2d9dc8e01d02d6e
RAS <r.a.s@live.ru>
2018-04-14 18:47:39 +0300

    refresh

--- 6_AngularJS HW/2_ModalTest/modalTest.js
+++ 6_AngularJS HW/2_ModalTest/modalTest.js
@@ -1,74 +1,97 @@
 var model = [{
-    q: 'Выберите директиву, которую можно использовать для двунаправленной привязки. ',
-    a1: 'ng-model-template',
-    a2: 'ng-template',
-    a3: 'ng-bind',
-    a4: 'ng-model !',
-    v: '4',
-    id: '1'
-  },
-  {
-    q: 'На странице размещено три блока текста, необходимо в зависимости от действия пользователя отображать один из этих блоков. С помощью какой директивы можно решить подобную задачу?',
-    a1: 'ng-switch!',
-    a2: 'ng-if',
-    a3: 'ng-options',
-    a4: 'ng-class',
-    v: '1',
-    id: '2'
-  },
-  {
-    q: 'Какая из директив использует AJAX запрос на сервер ? ',
-    a1: 'ng-switch',
-    a2: 'ng-include!',
-    a3: 'ng-ajax',
-    a4: 'ng-repeat',
-    v: '2',
-    id: '3'
-  },
-  {
-    q: 'На странице необходимо создать список на основе данных массива, какая директива позволяет выполнить данную задачу?',
-    a1: 'ng-list',
-    a2: 'ng-repeat!',
-    a3: 'ng-foreach',
-    a4: 'ng-switch',
-    v: '2',
-    id: '4'
-  },
-  {
-    q: 'Для того чтобы отменить привязку используется директива ',
-    a1: 'ng-no-model',
-    a2: 'ng-cancel-bind',
-    a3: 'ng-non-bindable!',
-    a4: 'ng-cancel-model',
-    v: '3',
-    id: '5'
-  }
+  q: 'Выберите директиву, которую можно использовать для двунаправленной привязки. ',
+  a1: 'ng-model-template',
+  a2: 'ng-template',
+  a3: 'ng-bind',
+  a4: 'ng-model ',
+  v: '4',
+  id: '1',
+  disabled: false
+},
+{
+  q: 'На странице размещено три блока текста, необходимо в зависимости от действия пользователя отображать один из этих блоков. С помощью какой директивы можно решить подобную задачу?',
+  a1: 'ng-switch',
+  a2: 'ng-if',
+  a3: 'ng-options',
+  a4: 'ng-class',
+  v: '1',
+  id: '2',
+  disabled: false
+},
+{
+  q: 'Какая из директив использует AJAX запрос на сервер ? ',
+  a1: 'ng-switch',
+  a2: 'ng-include',
+  a3: 'ng-ajax',
+  a4: 'ng-repeat',
+  v: '2',
+  id: '3',
+  disabled: false
+},
+{
+  q: 'На странице необходимо создать список на основе данных массива, какая директива позволяет выполнить данную задачу?',
+  a1: 'ng-list',
+  a2: 'ng-repeat',
+  a3: 'ng-foreach',
+  a4: 'ng-switch',
+  v: '2',
+  id: '4',
+  disabled: false
+},
+{
+  q: 'Для того чтобы отменить привязку используется директива ',
+  a1: 'ng-no-model',
+  a2: 'ng-cancel-bind',
+  a3: 'ng-non-bindable',
+  a4: 'ng-cancel-model',
+  v: '3',
+  id: '5',
+  disabled: false
+}
 ];
+
 var testApp = angular
   .module('testApp', [])
-  .controller('testCtrl', function ($scope) {
+  .controller('testCtrl', function ($scope, $window) {
     $scope.questions = model;
     $scope.url = '';
 
-    $scope.check = [];
+    $scope.checkQ = [];
     $scope.checked = false;
     $scope.radio = false;
+    $scope.disabled = false;
+    $scope.check = false;
+    $scope.count = 0;
+
     $scope.checkAll = function (i) {
       if ($scope.q.v === i) {
-        $scope.check.push($scope.q.id);
+        $scope.checkQ.push($scope.q.id);
+        $scope.count++;
         console.log(i);
         console.log($scope.q.id);
       }
-      $scope.i = true;
-     
+      $scope.disabled = true;
+
     };
 
     $scope.showList = function (i) {
-
-      $scope.checked = false;
-      $scope.check = false;
+      $scope.disabled = false;
       $scope.q = $scope.questions[i];
+      $scope.questions[i].disabled = true;
       $scope.url = 'page.html';
+      $scope.checked = function () {
+        return false;
+      };
     };
-    $scope.count = $scope.check.lenght;
+
+    $scope.result = function (i) {
+      $scope.url = 'result.html';
+      $scope.ans = $scope.checkQ.join(' ,');
+    };
+
+    $scope.reload = function () {
+      
+      $window.location.reload();
+      
+    };
   });
---
+++ Gulp/dist/js/main.js
@@ -0,0 +1,2 @@
+console.log("333.js"),console.log("111");
+//# sourceMappingURL=main.js.map
